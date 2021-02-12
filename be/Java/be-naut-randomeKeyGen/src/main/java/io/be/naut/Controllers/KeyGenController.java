d668c5fa007bf90c266b4f53a9ded2dc9bca9b5a
vipin-git-dotcom <67404930+vipin-git-dotcom@users.noreply.github.com>
2021-02-12 11:36:11 +0530

    NoobLevelRandomKeyGen

---
+++ be/Java/be-naut-randomeKeyGen/src/main/java/io/be/naut/Controllers/KeyGenController.java
@@ -0,0 +1,24 @@
+package io.be.naut.Controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.be.naut.service.KeyGenService;
+
+@RestController
+@RequestMapping(path = "/")
+public class KeyGenController {
+
+	KeyGenService service;
+
+	public KeyGenController(KeyGenService serivce) {
+		this.service = serivce;
+	}
+
+	@GetMapping
+	public String generateRandomKey() {
+		return service.generateKeyGen();
+	}
+
+}
---
+++ be/Java/be-naut-randomeKeyGen/src/main/java/io/be/naut/service/KeyGenService.java
@@ -0,0 +1,46 @@
+package io.be.naut.service;
+
+import java.security.SecureRandom;
+import java.util.Random;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class KeyGenService {
+
+	private static final int length = 8;
+	private static final char[] SYMBOLS = "@!%[$^}{&]:;>?<*)(#".toCharArray();
+	private static final char[] LOWERCASE = "yhqmxsacrwgeltviduofpbjknz".toCharArray();
+	private static final char[] UPPERCASE = "VDAWZUYGLXTKNESCMORHJQFBIP".toCharArray();
+	private static final char[] NUMBERS = "8175926304".toCharArray();
+	private static final char[] ALL_CHARS = "yhqmxsacrwgeltviduofpbjknz@!%$^&*)81{}[]:;<>?75926304(#VDAWZUYGLXTKNESCMORHJQFBIP"
+			.toCharArray();
+
+	static Random rand = new SecureRandom();
+
+	public String generateKeyGen() {
+		String stringPassword;
+		char[] password = new char[length];
+
+		password[0] = UPPERCASE[rand.nextInt(UPPERCASE.length)];
+		password[1] = LOWERCASE[rand.nextInt(LOWERCASE.length)];
+		password[2] = SYMBOLS[rand.nextInt(SYMBOLS.length)];
+		password[3] = NUMBERS[rand.nextInt(NUMBERS.length)];
+		// fill up the pass with random char
+		for (int i = 4; i < length; i++) {
+			password[i] = ALL_CHARS[rand.nextInt(ALL_CHARS.length)];
+		}
+		System.out.println("original password" + new String(password));
+		// shuffle password's character
+		for (int i = 0; i < password.length; i++) {
+			int randomPosition = rand.nextInt(password.length);
+			char temp = password[i];
+			password[i] = password[randomPosition];
+			password[randomPosition] = temp;
+
+		}
+		stringPassword = new String(password);
+		System.out.println("New Password" + stringPassword);
+		return stringPassword;
+	}
+}
